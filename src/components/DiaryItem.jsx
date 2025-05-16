2f731658137a9b9df1ee0900b622a8fc3f70a4e9
byjoon <byjoonlee@gmail.com>
2025-05-16 12:41:48 +0900

    DiaryItem log 보강

--- src/components/DiaryItem.jsx
+++ src/components/DiaryItem.jsx
@@ -5,6 +5,14 @@ import { useNavigate } from "react-router-dom";
 
 const DiaryItem = ({ id, emotionId, content, createdDate }) => {
   const nav = useNavigate();
+
+  console.log("DiaryItem 렌더링:", {
+    id,
+    emotionId,
+    content,
+    createdDate: new Date(createdDate).toLocaleString(),
+  });
+
   if (!id) {
     return <div>일기를 불러오는 중 : DiaryItem.jsx</div>;
   }
